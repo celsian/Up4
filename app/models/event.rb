df5f585fc5a376a3ad779077f35cc1bca2c624b5
Lucas Paolella <celsian@gmail.com>
2017-08-01 14:57:38 -0700

    Added error_messages method to event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,13 @@ class Event < ApplicationRecord
   has_many :users, through: :user_events
 
   validates_presence_of(:name, :time_date, :location)
+
+  def error_messages
+    messages = ""
+    errors.full_messages.each do |message|
+      messages += message + "."
+    end
+    messages
+  end
+
 end
