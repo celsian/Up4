28388fcaba15671aa88eb35b456e0a66179baad4
Lucas Paolella <celsian@gmail.com>
2017-08-12 14:45:44 -0700

    Hard coded an IP address for development testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def remote_ip
+    if request.remote_ip == '127.0.0.1'
+      # Hard coded remote address
+      Rails.application.secrets.ip
+    else
+      request.remote_ip
+    end
+  end
+
 end
