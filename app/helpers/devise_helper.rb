7f6ea7eccea8a9d7bd7b40e86078e0d258f9c1d4
Lucas Paolella <celsian@gmail.com>
2017-09-09 18:33:22 -0700

    Devise now uses my registrations controller to permit location, new devise flash notices.

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,29 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = I18n.t('errors.messages.not_saved',
+      count: resource.errors.count,
+      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="flash_messages">
+        <div class="alert alert-danger alert-dismissible" role="alert">
+          <button class="close" data-dismiss="alert">×</button>
+            <B>Error:</B> #{messages}
+        </div>
+    </div>
+    HTML
+
+    # html = <<-HTML
+    # <div class="alert alert-danger alert-block">
+    #   <button type="button" class="close" data-dismiss="alert">x</button>
+    #   <h4>#{sentence}<<<</h4>
+    #   #{messages}>>>>
+    # </div>
+    # HTML
+
+    html.html_safe
+  end
+end
